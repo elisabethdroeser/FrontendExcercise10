efc0c9b7ced1473711b89e0c17c0d1c4df1cd492
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2022-01-22 19:01:28 +0100

    First fetch request.

--- script.js
+++ script.js
@@ -4,21 +4,30 @@
 med hjälp av template literals (bakåtvända fnuttar). 
 Exempel: let s = `Value 1: ${value1}, Value 2: ${value2}`;
 */
-function getApi() {
-     
-    fetch('https://www.swapi.tech/api/people/?name')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err))
-     
+const btn = document.querySelector('#btn');
 
-/*
-    fetch(fullUri)
-        .then(res => res.json())
-        .then(data => {
-
-        })
-        .catch(err => console.log(err))
-        */
+const getApi = () => {  
+    fetch('https://www.swapi.tech/api/people/') 
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log('Error ' + err));
 }
+
+btn.addEventListener('click', getApi);
+     /*
+        .then(res => {
+            console.log(res); //Response {type: 'basic', url: 'http://127.0.0.1:5501/JS/ED.16Fetch/json.json', redirected: false, status: 200, ok: true, …}
+            //const data = res.json();
+            //console.log(data); 
+            return res.json();
+        })
+
+        .then(data => console.log(data));
+    fetch('https://www.swapi.tech/api/people/1')
+        .then(res => {
+            console.log(res)
+        })
+    }   */
 
